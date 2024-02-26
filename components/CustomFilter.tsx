5f2075240122593a47aefedd611ada907a73ae7e
ZhenyaKelarev <zeka416351@gmail.com>
2024-02-26 10:26:19 +0200

    add layout
    add hero
    add carCard
    add footer
    add utils

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,5 @@
+const CustomFilter = ({ title }: { title: string }) => {
+  return <div>CustomFilter</div>;
+};
+
+export default CustomFilter;
--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,19 @@
-import Hero from "./Hero";
+import CarCard from "./CarCard";
 import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, CustomButton };
+export {
+  CarCard,
+  CustomButton,
+  CustomFilter,
+  Footer,
+  Hero,
+  Navbar,
+  SearchBar,
+  SearchManufacturer,
+};
